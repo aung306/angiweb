7207369cffa9e4800ea7469d10e3dfe4e0cb94ba
Angela Ung <angelaung@ufl.edu>
2023-12-08 15:06:29 -0500

    update socials

--- app/routes/graphic._index.tsx
+++ app/routes/graphic._index.tsx
@@ -9,17 +9,33 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="lg:text-center py-16 bg-gray-100">
-            <h2 className="text-base font-serif mt-2 mb-6 text-3xl sm:text-4xl font-bold tracking-tight leading-7 text-pink-400">
+    <div className="bg-[url('../img/cherry.gif')] bg-no-repeat bg-cover text-center py-24 bg-gray-100">
+            <h1 className="text-5xl animate-fade-down text-base mt-24 mb-12 font-bold leading-7 text-pink-400">
               Angela Ung
-            </h2>
-            <ul className="mx-auto max-w-4xl py-4 divide-y divide-gray-100 rounded-md border border-gray-200 bg-white">
+            </h1>
+            <div className="animate-fade-down mt-6 mb-28 text-pink-900 text-lg font-medium">
+              <a className="m-6 p-2 hover:text-pink-400" href="/aboutme">
+                    about me
+              </a>
+              <a> ~ </a>
+              <a className="m-6 p-2 hover:text-pink-400" href="/socials">
+                    socials
+              </a>
+            </div>
+            <ul className="pr-6 pl-6 mx-auto max-w-6xl p-4 divide-y divide-gray-100 mb-20 rounded-md border border-gray-200 bg-white">
               <li>
                 <p
-                  className="font-semibold font-serif mt-3 text-lg h-10 leading-8 text-pink-400 mt-0"
+                  className="animate-fade-down font-semibold mt-6 mb-10 text-lg h-10 leading-8 text-pink-400 mt-0"
                 >
-                  Thanks for stopping by! Unfortunately this page is under construction.
+                  Hey! This page is currently under construction. Check my socials on how to contact me!
                 </p>
+              </li>
+              <li className="animate-fade-up text-center mb-10 content-center">
+                <div className="flex justify-center mt-6 mb-6 text-pink-900 text-lg font-medium">
+                  <a className=" m-3 p-2 pl-4 pr-4 rounded-md bg-pink-200 hover:text-pink-400" href="/">
+                  back
+                  </a>
+                </div>
               </li>
             </ul>
     </div>
