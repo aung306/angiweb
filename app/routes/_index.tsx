378de741a41a8cd363513525297131ed3c871fb5
Angela Ung <angelaung@ufl.edu>
2023-12-08 14:36:15 -0500

    add mobile friendly experience

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,8 +9,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="bg-[url('../img/cherry.gif')] bg-no-repeat bg-cover lg:text-center py-24 bg-gray-100">
-            <h1 className="animate-fade-down text-base font-serif mt-24 mb-12 text-7xl font-bold tracking-tight leading-7 text-pink-400">
+    <div className="bg-[url('../img/cherry.gif')] bg-no-repeat bg-cover text-center py-24 bg-gray-100">
+            <h1 className="animate-fade-down text-base mt-24 mb-12 text-5xl font-bold tracking-tight leading-7 text-pink-400">
               Angela Ung
             </h1>
             <div className="animate-fade-down mt-6 mb-36 text-pink-900 text-lg font-medium">
@@ -25,20 +25,20 @@ export default function Index() {
             <ul className="mx-auto max-w-6xl py-4 divide-y divide-gray-100 mb-20 rounded-md border border-gray-200 bg-white">
               <li>
                 <p
-                  className="animate-fade-down font-semibold font-serif mt-6 mb-6 text-lg h-10 leading-8 text-pink-400 mt-0"
+                  className="animate-fade-down font-semibold mt-6 mb-10 text-lg h-10 leading-8 text-pink-400 mt-0"
                 >
                   Welcome to my personal website! Click one of the tabs below to learn more about me.
                 </p>
               </li>
-              <li className="animate-fade-up text-center mb-20">
-                <div className="mt-12 mb-12 text-pink-900 text-lg font-medium">
+              <li className="animate-fade-up text-center mb-20 content-center">
+                <div className="flex justify-center mt-12 mb-12 text-pink-900 text-lg font-medium">
                   <a className="m-6 p-4 rounded-md bg-pink-200 hover:text-pink-400" href="/swe">
                   software engineering experience
                   </a>
                   <a className=" m-6 p-4 rounded-md bg-pink-200 hover:text-pink-400" href="/graphic">
                   graphic design portfolio
                   </a>
-                  <a className=" m-6 p-4 rounded-md bg-pink-200 hover:text-pink-400" href="/esports">
+                  <a className=" m-6 p-4 rounded-md bg-pink-200 hover:text-pink-400" href="/esport">
                   esports career
                   </a>
                 </div>
