544528ee5225b95dc3b2c7588604756e9eb9f159
Angela Ung <angelaung@ufl.edu>
2023-12-20 23:21:02 -0500

    update swe:internships

--- app/routes/swe._index.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "@remix-run/react";
-
-export default function SweIndex() {
-    return (
-      <div>
-            Click on a button to learn more!
-      </div>
-    );
-  }
--- app/routes/swe.tsx
+++ app/routes/swe.tsx
@@ -45,7 +45,7 @@ export default function Index() {
                   B.S. in Computer Science at <a href="https://www.ufl.edu/" className="underline text-pink-700">University of Florida</a>'s <a href="https://www.eng.ufl.edu/" className="underline text-pink-700">College of Engineering</a>
                 </p>
                 <p
-                  className="animate-fade-down animate-pulse m-16 text-sm h-10 leading-8 text-pink-400"
+                  className="animate-fade-down m-16 text-sm h-10 leading-8 text-pink-400"
                 >
                   No. 1 Public University of 2023 - <a href="https://www.wptv.com/news/state/university-of-florida-ranked-no-1-public-school-in-nation-by-wall-steet-journal" className="underline text-pink-700">Wall Street Journal</a>
                 </p>
@@ -57,37 +57,40 @@ export default function Index() {
               </li>
               <li>
                 <p id="intern"
-                  className="animate-fade-up font-['Vivaldi'] m-10 text-3xl text-brown-800"
+                  className="animate-fade-down font-['Vivaldi'] m-10 text-3xl text-brown-800"
                 >
                   Internships
                 </p>
                 <p
-                  className="animate-fade-up font-semibold m-12 text-xl leading-8 text-pink-400"
+                  className="animate-fade-down font-semibold m-12 text-xl leading-8 text-pink-400"
                 >
                   Ultimate Kronos Group (UKG) - Software Engineering Intern
                 </p>
                 <p
-                  className="animate-fade-up animate-pulse mb-10 m-6 text-md leading-8 text-pink-400"
+                  className="animate-fade-down animate-pulse m-6 text-md leading-8 text-pink-400"
                 >
                   What did I do there?
                 </p>
                 <div className="ease-in leading-12">
-                  <p
-                   className="animate-fade-up mb-2 m-16 text-md leading-8 text-pink-500">
-                    During Summer of 2023, I interned for 13 weeks in UKG's Development Acceleration team.
-                    I eagerly took the initiative to lead my team's daily stand-up meetings as soon as I understood production in an Agile environment.
-                    I gained several skills such as CI/CD pipelines, the SDLC, REST APIs, Kubernetes & Docker, Git, and other essentials when I spearheaded projects assigned to me.
-
-                  </p>
-                  <p
-                   className="animate-fade-up mb-10 text-md leading-8 text-pink-600">
-                    Check out my internship projects below!
-                  </p>
+                <div className="animate-fade-down flex justify-center mt-6 mb-4 text-pink-900 text-md font-medium">
+                    <a className="hover:animate-bounce m-3 p-2 pl-4 pr-4 rounded-md bg-pink-200 hover:text-pink-400" href="#projects">
+                    click here to see my internship projects!
+                    </a>
+                </div>
+                <p
+                   className="animate-fade-up m-16 mb-4 text-md p-3 font-semibold leading-10 text-pink-500">
+                    <a className="text-4xl font-['Arial']">" </a>
+                    Angela took and delivered on an actual project where she built the frontend for a new internal tool. 
+                    She had to learn a new tech stack; she took that up as a challenge and was able to learn it very quickly and was able to start contributing in just a couple of days. 
+                    She also did a demo of the project to a large audience that was very well received.           
+                    <a className="text-4xl font-['Arial']"> "</a>     
+                </p>
+                <p className="text-pink-600 animate-fade-up font-semibold mb-16">- <a href="https://www.linkedin.com/in/kaushaldalvi/" className="underline text-pink-700">Kaushal Dalvi</a></p>
                 </div>
               </li>
               <li>
                 <p id="projects"
-                  className="animate-fade-down font-['Vivaldi'] m-10 text-3xl text-brown-800"
+                  className="animate-fade-up font-['Vivaldi'] m-10 text-3xl text-brown-800"
                 >
                   Projects
                 </p>
