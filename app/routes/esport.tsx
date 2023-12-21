1a5cdc20ec65606a1ad00b460c8f4f95daf7a875
Angela Ung <angelaung@ufl.edu>
2023-12-21 05:00:28 -0500

    esport update

--- app/routes/esport.tsx
+++ app/routes/esport.tsx
@@ -24,11 +24,37 @@ export default function Index() {
             </div>
             <ul className="pr-6 pl-6 mx-auto max-w-6xl p-4 divide-y divide-gray-200 mb-20 rounded-md border border-gray-300 bg-white">
               <li>
-                <p
-                  className="animate-fade-down font-semibold mt-6 mb-10 text-lg h-10 leading-8 text-pink-400 mt-0"
+              <p id="education"
+                  className="animate-fade-down font-['Vivaldi'] m-10 mb-2 text-3xl text-brown-800"
                 >
-                  Hey! This page is currently under construction. Check my socials on how to contact me!
+                  Valorant
                 </p>
+              <p className="animate-fade-down p-4 text-pink-700">I am actively seeking a spot on a <a className="font-bold">NA Game Changers Valorant </a> team. </p>
+              <div className="p-4 animate-fade-down flex justify-center mb-6 text-pink-900">
+                  <div className="text-left leading-16 p-2 pr-10 pl-10 mx-auto max-w-5xl p-4 divide-gray-300 rounded-md border border-gray-300 bg-pink-50">
+                      <p><a href="https://tracker.gg/valorant/profile/riot/angi%23chi/overview" className="font-bold">Peak Rank: </a>Ascendant 1</p>
+                      <p><a className="font-bold">Primary Role: </a>Sentinel & Smokes</p>
+                  </div>
+              </div>
+              <p className="animate-fade-up font-semibold text-xl leading-8 text-pink-400">
+                    Competitive Scene Experience
+              </p>
+              <div className="p-4 animate-fade-up flex justify-center mb-6 text-pink-900">
+                  <ul className="pr-10 pl-10 mx-auto max-w-5xl p-4 divide-y divide-gray-300 rounded-md border border-gray-300 bg-pink-50">
+                    <li className="font-semibold mb-2 text-xl">
+                      <a>UFFSA WolfPack Tournament</a>
+                    </li>
+                    <li className="p-4">
+                      <p className="">University tournament competing for $1000 donation to charity of choice </p>
+                    </li>
+                    <li className="text-left leading-16 p-4 pl-12 pr-12">
+                      <p><a className="font-bold">Placement: </a>Semifinalists</p>
+                      <p><a className="font-bold">Team Name: </a>BWTHB</p>
+                      <p><a className="font-bold">Role: </a>IGL</p>
+                      <p><a className="font-bold">Key Notes: </a>Forfeited due to a member catching Covid </p>
+                    </li>
+                  </ul>
+                </div>
               </li>
               <li className="animate-fade-up text-center mb-10 content-center">
                 <div className="flex justify-center mt-6 mb-6 text-pink-900 text-lg font-medium">
